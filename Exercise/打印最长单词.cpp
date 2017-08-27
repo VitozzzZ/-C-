ff5465388937e33e88a6e5c1716642a810280d45
VitozzzZ <zealucius@gmail.com>
2017-08-27 16:22:33 +0800

    Create 打印最长单词.cpp

---
+++ Exercise/打印最长单词.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char str[510];
+    string s;
+    string maxs = "";
+    int m = 0;
+    int i = 0;
+    cin.get(str, 510);
+    s=string(str);
+    for (i=1; str[i]!='\0'; i++)
+    {
+        if (str[i] == ' ' or str[i] == '.')
+        {
+            string tmp = s.substr(m, i-m);
+            if (tmp.size() > maxs.size())
+            {
+                maxs = tmp;
+            }
+            m = i + 1;
+        }
+    }
+    cout << maxs << endl;
+    
+    return 0;
+}
